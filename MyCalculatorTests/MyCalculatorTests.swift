115c4ff3fa2369a19fb0fe8f9fabc34a3cca4d08
bhrigu sehra <bhrigusehra1448@gmail.com>
2021-08-20 10:13:45 +0530

    Initial Commit

---
+++ MyCalculatorTests/MyCalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyCalculatorTests.swift
+//  MyCalculatorTests
+//
+//  Created by Bhrigu Sehra on 8/20/21.
+//  Copyright © 2021 Bhrigu Sehra. All rights reserved.
+//
+
+import XCTest
+@testable import MyCalculator
+
+class MyCalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
